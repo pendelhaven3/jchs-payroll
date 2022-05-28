369f580fae5951a43d1ec942fc44c56d785794f6
PJ Miranda <pendelhaven3@gmail.com>
2022-05-28 08:37:33 +0800

    Save initial data for Philhealth/Pagibig

--- alterscripts/alterscript-1.3.0.sql
+++ alterscripts/alterscript-1.3.0.sql
@@ -3,3 +3,6 @@ alter table salary add payType varchar(20);
 update salary set paySchedule = 'WEEKLY', payType = 'PER_DAY';
 alter table salary modify column paySchedule varchar(20) not null;
 alter table salary modify column payType varchar(20) not null;
+
+insert into philhealthcontributiontable (id, floor, ceiling, multiplier, householdMonthlyContribution) values (1, 10000, 40000, 2.75, 0);
+insert into systemparameter (id, name, value) values (1, 'PAGIBIG_CONTRIBUTION_VALUE', 100);
