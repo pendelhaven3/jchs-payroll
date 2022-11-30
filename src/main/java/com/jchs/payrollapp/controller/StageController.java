f401de3bd8aab09419e6bfa6039bb6917b861d69
PJ Miranda <pendelhaven3@gmail.com>
2022-11-30 23:20:42 +0800

    Update version to 1.4.0

--- src/main/java/com/jchs/payrollapp/controller/StageController.java
+++ src/main/java/com/jchs/payrollapp/controller/StageController.java
@@ -104,7 +104,7 @@ public class StageController {
 	}
 	
 	public void setTitle(String title) {
-		stage.setTitle("JCHS Payroll App 1.3.6 - " + title);
+		stage.setTitle("JCHS Payroll App 1.4.0 - " + title);
 	}
 
 	public void showMainMenuScreen() {
